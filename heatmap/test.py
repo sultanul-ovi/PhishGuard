a9eb155bc744037f351ea911dae17d665015e569
sultanul-ovi <sultanulislamovi@gmail.com>
2024-09-11 00:20:49 -0400

    org

--- heatmap/test.py
+++ heatmap/test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import os
 
 # Load the Excel file
-file_path = 'heatmap/sorted_model_results6.xlsx'  # Replace with your Excel file path
+file_path = 'heatmap/s1.xlsx'  # Replace with your Excel file path
 data = pd.read_excel(file_path)
 
 # Clean column names by stripping leading/trailing whitespaces
@@ -15,7 +15,7 @@ data['ML Model'] = data['ML Model'].str.strip()
 
 # Remove percentage signs, if any, and multiply the metrics by 100, round to 3 decimal points
 for col in ['Accuracy', 'F1 Score', 'Recall', 'Precision']:
-    data[col] = (data[col] * 100).round(3)
+    data[col] = (data[col] * 100).round(2)
 
 # Set the Model as the index for easier plotting
 data.set_index('ML Model', inplace=True)
@@ -26,16 +26,16 @@ metrics = ['Accuracy', 'F1 Score', 'Recall', 'Precision']
 # Generate the heatmap
 plt.figure(figsize=(14, 6))  # Set the size of the plot
 sns.heatmap(data[metrics], annot=True, cmap='summer_r', linewidths=0.5, fmt=".3f", cbar=False,
-            annot_kws={"fontweight": "bold"})  # Make the annotation text bold
+            annot_kws={"fontweight": "bold", "fontsize": 16})  # Increase font size and make it bold
 
-# Customize the heatmap
-plt.title('Model Performance Metrics Comparison on Dataset 06', fontweight='bold', fontsize=16)
-plt.xlabel('Metrics', fontweight='bold', fontsize=12)
-plt.ylabel('Models', fontweight='bold', fontsize=12)
+# # Customize the heatmap
+# plt.title('Machine Learning Model Metrics Comparison for Dataset 01', fontweight='bold', fontsize=18)
+plt.xlabel('Metrics', fontweight='bold', fontsize=14)
+plt.ylabel('Models', fontweight='bold', fontsize=14)
 
 # Set bold font for tick labels
-plt.xticks(fontweight='bold', fontsize=10)
-plt.yticks(fontweight='bold', fontsize=10)
+plt.xticks(fontweight='bold', fontsize=14)
+plt.yticks(fontweight='bold', fontsize=14)
 
 # Save the heatmap with the Excel file's name
 image_filename = os.path.splitext(os.path.basename(file_path))[0] + '_heatmap.png'
